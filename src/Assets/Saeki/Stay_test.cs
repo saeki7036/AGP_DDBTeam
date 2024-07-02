18f979ea86cdee3a427ccb666628d1eed8e997b8
saeki7036 <114328780+saeki7036@users.noreply.github.com>
2024-07-02 15:02:38 +0900

    UpDate/Enemy

---
+++ src/Assets/Saeki/Stay_test.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stay_test : Enemy_BeseTest
+{
+    public override void SetUp()
+    {
+        A = 4;
+    }
+}
