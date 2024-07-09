87d3a604b685878535c022b63042f3a3779bb599
Ton-1211 <113411399+Ton-1211@users.noreply.github.com>
2024-07-09 14:33:06 +0900

    Add/WeaponDataBase

---
+++ src/Assets/Suzuki/Scripts/DataBase/Bullet/BulletDataBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "OriginalScriptableObjects/DataBase/BulletDataBase")]
+public class BulletDataBase : BaseDataBase<BulletData>
+{
+
+}
--- src/Assets/Suzuki/Scripts/DataBase/Character/CharacterData.cs
+++ src/Assets/Suzuki/Scripts/DataBase/Character/CharacterData.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "OriginalScriptableObjects/Data/CharacterStatus")]
-public class CharacterStatusData : BaseData
+[CreateAssetMenu(menuName = "OriginalScriptableObjects/Data/CharacterData")]
+public class CharacterData : BaseData
 {
     [SerializeField] float maxHp;
-    [SerializeField] float maxPossesTime;
+    [SerializeField] float maxPossessTime;
 
     // プロパティ
     public float MaxHp
     {
         get { return maxHp; }
     }
-    public float MaxPossesTime
+    public float MaxPossessTime
     {
-        get { return maxPossesTime;}
+        get { return maxPossessTime;}
     }
 }
