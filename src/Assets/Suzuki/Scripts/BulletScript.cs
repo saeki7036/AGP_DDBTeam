a230d63001183516ad54dd4d31d4cdb8f70fab7e
Ton-1211 <113411399+Ton-1211@users.noreply.github.com>
2024-07-16 14:32:09 +0900

    Update/Bullet

--- src/Assets/Suzuki/Scripts/BulletScript.cs
+++ src/Assets/Suzuki/Scripts/BulletScript.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// BulletBaseClassに移行
+/// </summary>
 public class BulletScript : MonoBehaviour
 {
     [SerializeField] BulletData bulletData;
+    [Header("弾が衝突するレイヤー"), SerializeField] LayerMask layerMask;
     void Start()
     {
      
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.TryGetComponent<CharacterStatus>(out CharacterStatus character))// キャラクターに当たったとき
+        if (other.gameObject.layer == layerMask)
         {
-            character.TakeDamage(bulletData.AttackPower);
-            Destroy(this.gameObject);
+            if (other.TryGetComponent<CharacterStatus>(out CharacterStatus character))// キャラクターに当たったとき
+            {
+                character.TakeDamage(bulletData.AttackPower);
+                Destroy(this.gameObject);
+            }
         }
     }
 }
