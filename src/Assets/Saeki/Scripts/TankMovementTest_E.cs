f886d6547a60897038a55669084a456bd8f4c05c
saeki7036 <114328780+saeki7036@users.noreply.github.com>
2024-10-21 08:41:04 +0900

    UpDate/Script

--- src/Assets/Saeki/Scripts/TankMovementTest_E.cs
+++ src/Assets/Saeki/Scripts/TankMovementTest_E.cs
@@ -1,40 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
 
 public class TankMovementTest_E : EnemyBaseClass
 {
     [SerializeField]
     GameObject TankBody, TankCanon, TankCatapera;
+    private int cornersCount = 0;
+    private Vector3 directionToNext;
+    void BodyRotete()
+    {
+        Vector3 directionToTarget = Target.transform.position - TankBody.transform.position;
+        directionToTarget.y = 0; // 水平方向だけに向く場合、Y軸の回転を無視する
+        LookSlerp(TankBody, directionToTarget);
+    }
     void CataperaRotete()
     {
-        Vector3 directionToTarget = Target.transform.position - transform.position;
-        directionToTarget.y = 0; // 水平方向だけに向く場合、Y軸の回転を無視する
+        var corners = Agent.path.corners;
+        if (cornersCount == corners.Length)
+        {
+            cornersCount = corners.Length;
+            directionToNext = corners[1] - corners[0];
+            directionToNext.y = 0; // 水平方向だけに向く場合、Y軸の回転を無視する
+        }
+        LookSlerp(TankCatapera, directionToNext);
+    }
+    void CanonRotete()
+    {
+        Vector3 directionToTarget = Target.transform.position - TankCanon.transform.position;
+        directionToTarget.x = 0;
+        LookSlerp(TankCanon,directionToTarget);
+    }
 
-        // 現在の前方向とターゲットの方向の角度を計算
-        float angle = Vector3.Angle(transform.forward, directionToTarget);
+    void LookSlerp(GameObject tankPart,Vector3 dir)
+    {
+        float angle = Vector3.Angle(tankPart.transform.forward, dir);
 
         // 一定以上の角度差があれば、ターゲットの方向を向く
         if (angle > 0.1f)
         {
             // ターゲット方向に向けた回転を計算
-            Quaternion targetRotation = Quaternion.LookRotation(directionToTarget);
+            Quaternion targetRotation = Quaternion.LookRotation(dir);
 
             // 徐々にターゲットの方向に回転
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            tankPart.transform.rotation = Quaternion.Slerp(tankPart.transform.rotation,
+                targetRotation, rotationSpeed * Time.deltaTime);
         }
     }
-
     protected override void StopChase()
     {
+        CataperaRotete();
+
         if (FindCheck())
         {
             Agent.speed = 0f;
             // ターゲットの方向への回転
-            Vector3 direction = Target.transform.position - transform.position;
-            direction.y = 0.0f;
-            Quaternion lookRotation = Quaternion.LookRotation(direction, Vector3.up);
-            transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+            BodyRotete();
+            CanonRotete();
 
             remainingCount += Time.deltaTime;
 
