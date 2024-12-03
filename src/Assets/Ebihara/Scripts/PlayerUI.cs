69b1ec5c6747cfa947d07297202198b646bce384
ebifry22 <g2227011@st.t-kougei.ac.jp>
2024-12-03 13:55:31 +0900

    add/PlayerUIPrefab

--- src/Assets/Ebihara/Scripts/PlayerUI.cs
+++ src/Assets/Ebihara/Scripts/PlayerUI.cs
@@ -7,27 +7,16 @@ public class PlayerUI : MonoBehaviour
 {
     Change change;
     [SerializeField] PlayerDamageEffect damageEffect;
-    float oldHp;
 
     // Start is called before the first frame update
     void Start()
     {
         change = GameObject.FindObjectOfType<Change>();
-        oldHp = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (oldHp != change.CharacterStatusHp)
-        {
-            //ƒ_ƒ[ƒW‚ğó‚¯‚½‚É”í’e‚Ìˆ—
-            Debug.Log("HP:" + change.CharacterStatusHp);
-
-            //damageEffect.DamageEffect(change.CharacterStatusHp);
-        }
         damageEffect.DamageEffect(change.CharacterStatusHp);
-
-        oldHp = change.CharacterStatusHp;
     }
 }
